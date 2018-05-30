6e13de454cf3a333a7cf118386df929f838c68bb
Shweyin <shweyin@gmail.com>
2018-05-29 21:04:06 -0400

    sdff

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var mydata = require('data-service.js');
+var mydata = require('data-service.js');
 
 var app = express();
 
@@ -10,21 +10,28 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
     res.sendfile('views/home.html');
 });
-app.get('/about.html', function(req, res)
+app.get('/home', function(req, res){
+    res.sendfile('views/home.html');
+});
+app.get('/about', function(req, res)
 {
     res.sendfile('views/about.html');
 });
 app.get('/employees', function(req, res)
 {
-    res.sendfile('views/about.html');
+    res.send('TODO: /employees get');
 });
 app.get('/managers', function(req, res)
 {
-    res.sendfile('views/about.html');
+    res.send('TODO: /managers get');
 });
 app.get('/departments', function(req, res)
 {
-    res.sendfile('views/about.html');
+    res.send('TODO: /departments get');
+});
+app.get('*', function(req, res)
+{
+    res.send('Sorry, this page does not exist(It would be awesome if it did though)', 404);
 });
 function onhttpstart(){
     console.log('server listening on port: ' + HTTP_PORT);
